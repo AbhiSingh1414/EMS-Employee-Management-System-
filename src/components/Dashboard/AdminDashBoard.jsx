89f3e5074740985b08d33dcd9c381b9b598999ab
Abhishek Singh <as1414abhisingh@gmail.com>
2026-05-30 00:26:16 +0530

    Added header in AdminDashboard

--- src/components/Dashboard/AdminDashBoard.jsx
+++ src/components/Dashboard/AdminDashBoard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Header from '../Cards/Header'
 
 const AdminDashBoard = () => {
   return (
-    <div className='h-screen w-full bg-[#1c1c1c] flex items-center justify-center '>
-      <div className=' h-[90%] w-[40%] rounded-2xl bg-[#1c1c1c] px-10 py-12 text-white shadow-[0_10px_30px_rgba(0,0,0,0.4)] border border-[#4f8fbf]'>
-
-      </div>
+    <div className='h-screen w-full bg-[#1c1c1c] text-white '>
+      <Header/>
     </div>
   )
 }
